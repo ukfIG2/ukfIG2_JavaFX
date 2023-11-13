1aa6d61ec60b7f8c0a96fd1dc84d8b5d493a4ca3
Gabris Ivan <ivan.gabris2@student.ukf.sk>
2023-11-13 20:55:13 +0100

    2023.11.13_Moorhun_pozadie_hotove

--- _08.3_Moorhuun_na_odovzdanie/src/application/Main.java
+++ _08.3_Moorhuun_na_odovzdanie/src/application/Main.java
@@ -16,8 +16,11 @@ public class Main extends Application{
 	private static Screen obrazovka = Screen.getPrimary();
 	private static final double Sirka_obrazovky = obrazovka.getBounds().getWidth();
     private	static final double Vyska_obrazovky = obrazovka.getBounds().getHeight();
+    //Nacirat pozadia
+    Image Pozadie_Oblaky = new Image("cloud.gif");
+    Image Pozadie_Dedinka = new Image("parallaxbackground.GIF");
     //Nastavit sirku hry
-    private static final double Sirka_hry = 3000;
+    private double Sirka_hry = Pozadie_Dedinka.getWidth()*2 ;	//sirka Snimky/parallaxbackground.GIF *2
     //Premenne pre pozadie
     private ImageView pozadie_oblaky;
     private ImageView pozadie_dedinka;
@@ -32,18 +35,14 @@ public class Main extends Application{
 		Scene scene = new Scene(root, Sirka_obrazovky, Vyska_obrazovky);	//Nastavenie sceny
 		
 		//Nastavenie pozadi
-		Image Pozadie_Oblaky = new Image("cloud.gif");
 		pozadie_oblaky = new ImageView(Pozadie_Oblaky);
-		pozadie_oblaky.setFitWidth(Sirka_obrazovky);
+		pozadie_oblaky.setFitWidth(Sirka_hry);
 		pozadie_oblaky.setFitHeight(Vyska_obrazovky);
 		root.getChildren().add(pozadie_oblaky);
 		
-		Image Pozadie_Dedinka = new Image("parallaxbackground.GIF");
-		//System.out.println(Pozadie_Dedinka.getHeight());
 		pozadie_dedinka = new ImageView(Pozadie_Dedinka);
-		//System.out.println(pozadie_dedinka.getFitHeight());
 		pozadie_dedinka.setFitHeight(Vyska_obrazovky);
-		//System.out.println(pozadie_dedinka.getFitHeight());
+		pozadie_dedinka.setFitWidth(Sirka_hry);
 		pozadie_dedinka.setY(Vyska_obrazovky-pozadie_dedinka.getFitHeight());
 		root.getChildren().add(pozadie_dedinka);
 		
@@ -56,27 +55,31 @@ public class Main extends Application{
 		primaryStage.getIcons().add(icon);			//Nastavenie icony pre panel
 		
 		primaryStage.setTitle("Moorhun GI");
-		//rimaryStage.setFullScreen(true);			//Nastavy FullScreen mode
+		//primaryStage.setFullScreen(true);			//Nastavy FullScreen mode
 		//primaryStage.setResizable(false);			//Znemozny menit velkost okna 
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-		//Nastavenie udalosti pohubu mysi
-		scene.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> pohniKamerou(event.getX(), event.getY()));
+		//Nastavenie udalosti pohybu mysi
+		scene.addEventHandler(MouseEvent.MOUSE_MOVED, event -> pohniKamerou(event.getX()));
 	}
 	
-	private void  pohniKamerou(double mouseX, double mouseY) {
-		if (mouseX <= 50) {
-            // Move the view to the left
+	private void  pohniKamerou(double mouseX) {
+		//System.out.println(mouseX);
+		//System.out.println(root.getTranslateX());
+		double priestor_aktivity = 200;
+		if (mouseX <= priestor_aktivity) {			//Preco 200? Lebo.
+            // Pohyb dolava
             if (root.getTranslateX() < 0) {
                 root.setTranslateX(root.getTranslateX() + rychlost_sceny);
             }
-        } else if (mouseX >= Sirka_obrazovky - 50) {
-            // Move the view to the right
+        } else if (mouseX >= Sirka_obrazovky - priestor_aktivity) {
+            // Pohyb doprava
             if (root.getTranslateX() > Sirka_obrazovky - Sirka_hry) {
                 root.setTranslateX(root.getTranslateX() - rychlost_sceny);
             }
         }
+		System.out.println(root.getTranslateX());
 	}
 
 	public static void main(String[] args) {
